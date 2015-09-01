1536de2e6f8eebd89604631fddde7ed2b0c696c6
Thorbjørn Wolf <thorbjorn.wolf@gmail.com>
2015-09-01 23:25:04 +0900

    Switched to argparse - optparse is deprecated

--- examprepper.py
+++ examprepper.py
@@ -1,6 +1,5 @@
 # encoding: utf-8
 
-import optparse
 from interfaces.terminal import Terminal
 
 def run(interface, file_path, media_path_rel='./media', presets=None):
@@ -18,39 +17,39 @@ def run(interface, file_path, media_path_rel='./media', presets=None):
     qc.run(interface)
 
 if __name__ == '__main__':
-    parser = optparse.OptionParser("usage: %prog interface_type [options]")
-    parser.add_option("-i", "--interface", dest="interface", type="string",
-                      default="terminal", help="Interface type. Currently only supporting 'terminal' for a terminal interface.")
-    parser.add_option("-f", "--file", dest="file_path", type="string",
-                      help="Absolute path to quiz file")
-    parser.add_option("-m", "--media", dest="media_path", type="string",
-                      default="./media", help="Relative or absolute path to media folder")
+    import argparse
     
-
-    parser.add_option("-r", "--repetition_lag", dest="repetition_lag", type="int",
+    parser = argparse.ArgumentParser(description="CLI for starting the examprepper")
+    parser.add_argument("-i", "--interface", dest="interface",default="terminal", 
+                        help="Interface type. Currently only supporting 'terminal' for a terminal interface.")
+    parser.add_argument("-f", "--file", dest="file_path", required=True,
+                        help="Absolute path to quiz file")
+    parser.add_argument("-m", "--media", dest="media_path",
+                        default="./media", help="Relative or absolute path to media folder")
+    
+    parser.add_argument("-r", "--repetition_lag", dest="repetition_lag", type=int,
                       default=None, help="Preset. Optional. How far ahead in the queue a failed question should be placed.")
-    parser.add_option("-c", "--categories", dest="category_indices", type="string",
+    parser.add_argument("-c", "--categories", dest="category_indices",
                       default=None, help="Preset. Optional list of category indices to use")
-    parser.add_option("-o", "--order", dest="order", type="string",
+    parser.add_argument("-o", "--order", dest="order",
                       default=None,
                       help="""Preset. Optional. Whether and how question order should be randomized. 
                       Choose among random, no_random, random_within_category, 
                       random_between_category, and
                       categories_random_and_random_within_category""")
 
-    options, args = parser.parse_args()
-
+    args = parser.parse_args()
     
     presets = dict()
-    if options.order: presets['order'] = options.order
-    if options.category_indices: presets['category_indices'] = [int(x) for x in options.category_indices.split(',')]
-    if options.repetition_lag: presets['repetition_lag'] = options.repetition_lag
+    if args.order: presets['order'] = args.order
+    if args.category_indices: presets['category_indices'] = [int(x) for x in args.category_indices.split(',')]
+    if args.repetition_lag != None: presets['repetition_lag'] = args.repetition_lag
 
     interface = None
-    if options.interface == 'terminal':
+    if args.interface == 'terminal':
         interface = Terminal()
     else:
-        print options.interface, "not an implemented interface type"
+        print args.interface, "not an implemented interface type"
         raise NotImplementedError()
 
-    run(interface, options.file_path, options.media_path, presets=presets)
+    run(interface, args.file_path, args.media_path, presets=presets)
